e2ba611e87d15a0db66b33ca53219ea2c40106b1
hubing <hs9800@163.com>
2015-04-21 22:43:18 +0800

    all——20150420

--- application/views/home/home_view.php
+++ application/views/home/home_view.php
@@ -1,7 +1,29 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: bing
- * Date: 2015-04-20
- * Time: 21:42
- */
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="gbk">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge",Chrome=1" >
+    <meta http-equiv="X-UA-Compatible" content="IE=9" />
+    <!--boostrap-->
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lte IE 9]>
+    <script src="bootstrap/js/respond.min.js"></script>
+    <script src="bootstrap/js/html5.js"></script>
+    <![endif]-->
+
+    <script src="http://libs.baidu.com/jquery/1.10.2/jquery.js"></script>
+    <script type="text/javascript">
+        $(function () {
+            // Invoke the plugin
+            $('input, textarea').placeholder();
+        });
+    </script>
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
